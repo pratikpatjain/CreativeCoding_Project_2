33981ddd64f2421d9bce0e837f13eee0e1ec3603
pratik jain <pratikjain@172-16-29-123.DYNAPOOL.NYU.EDU>
2017-11-27 18:06:37 -0500

    Final

--- Project2/p5.js
+++ Project2/p5.js
@@ -10,79 +10,126 @@ You may use graphics, but you are to mainly use image and text and to choreograp
 
 */
 
-var img, img2;
-var x, y, rotation;
-var textX, textY, siz; 
+var img
+var img2;
+var img3;
+var img4;
+var img5;
+var img6;
+var img7;
+var img8;
+var img9;
+var img10;
+var img11;
+var x = 0;
+var y = 2000;
+var rotation;
 var string1;
 var string2;
 var string3;
 var string4;
-var string5;
-var string6;
+var cx = 2500;
+var cy = 2000;
+var r = 150;
+var a = 3.14;
+var count = 0;
 
 //var destination;
 
 function preload() {
 
-  img = loadImage("data/background.gif");
+  img = loadImage("data/background.png");
   img2 = loadImage("data/spaceship.png");
+  img3 = loadImage("data/planetwithring.png");
+  img4 = loadImage("data/gasgiant.png");
+  img5 = loadImage("data/iceball.png");
+  img6 = loadImage("data/ringsplanet.png");
+  img7 = loadImage("data/dirtball.png");
+  img8 = loadImage("data/aquaball.png");
+  img9 = loadImage("data/lavaball.png");
+  img10 = loadImage("data/centerhole.png");
+  img11 = loadImage("data/jupiball.png");
   nova = loadFont("data/font0.otf");
 }
 
 function setup() {
-  createCanvas(600, 600);
-  //img = createImg("data/background.gif");
-  //destination = createImage(source.width, source.height);
+  createCanvas(900, 720);
   frameRate(15);
-  x = 0.0;
-  y = width/4.0;
   rotation = 0.0;
-  textX = 400;
-  textY = height;
-  siz = 100;
 
-  string1 = "ASTEROID WARS";
-  string2 = "The Myst";
-  string3 = "A long long time ago"
-  string4 = "In an asteroid belt far far away";
-  string5 = "A wormhole opens up in the heart of the belt";
-  string6 = "A team of scientists are sent on a spaceship to investigate."
-  
-  textY = textY-6;
-  siz++;
-
-  if (textY<0)
-  {
-    textY=height;
-    siz=100;
-  }
+  string1 = "The Mysterious Vortex";
+  string2 = "In a planetary belt far far away";
+  string3 = "A wormhole opens up in the heart of the belt";
+  string4 = "A team of scientists are sent on a spaceship to investigate.";
 
 }
 
 function draw() {
-  image(img,0,0);
 
-  //img.position(0,0);
-  
-  push();
-    translate(200, 100);
-    scale(0.3);
-    rotate(rotation);
-    image(img2, x, 0);
-  pop();
+  image(img,0,0); //Background Image
 
-  x += 1.0;
-  rotation += 0.01;
-  
-  if( x > width) x = 0;
-  if(rotation > 5) rotation = 0;
+  image(img10, 500+sin(frameCount)*3, //BlackHole Center
+        400+cos(frameCount*0.25)*1);
 
-  //text();
+  image(img3, 75+sin(frameCount*0.05)*7,
+        75+cos(frameCount*0.05)*7);
 
-  //textFont(font0);  
+  image(img4, 480+sin(frameCount*0.08)*100,
+        340+cos(frameCount*0.08)*75);
+
+  image(img8, 555+sin(frameCount*0.09)*80,
+        375+cos(frameCount*0.09)*80);
+
+  image(img6, 605+sin(frameCount*0.08)*10,
+        570+cos(frameCount*0.08)*31);
+
+  image(img7, 25+sin(frameCount*0.08)*5,
+        350+cos(frameCount*0.08)*11);
+
+  image(img5, 775+sin(frameCount*0.08)*2,
+        50+cos(frameCount*0.08)*2);
+
+  image(img9, 220+sin(frameCount*0.08)*11,
+        560+cos(frameCount*0.08)*10);
+
+  image(img11, 75+sin(frameCount*0.08)*1,
+        650+cos(frameCount*0.08)*1);
+
+  textFont(nova);
+  strokeWeight(2);
+  fill(215+ sin(frameCount*0.1)*100,215,0);
+  textSize(24);
+  text(string1,200,50);
+  textSize(16);
+  count++;
+  if(count > 40)
+  text(string2,250,100);
+  if(count > 80)
+  text(string3,300,135);
+  if(count > 120)
+  text(string4,350,170);
+
+
+  scale(0.2);
+  if (x < 2300){
+    image(img2, x, y);
+    x = x + 20;
+    y = y + random(-7,7);
+  }
+
+  else {
+
+    spiral();
+  }
 }
 
+function spiral(){
 
-
+var x = cx + r * cos(a);
+var y = cy + r * sin(a);
+image(img2, x, y);
+a = a + 0.1;
+console.log(a);
+}
 
 
